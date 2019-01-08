1125dcc9721a38ad0de3ab69f7e1d5fa4157ccfd
parkminho <minho4377@daum.net>
2019-01-09 02:25:35 +0900

    #3 First steps with Typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nicolas", age = 4, gender = "male";
+const sayhi = (name, age, gender) => {
+    console.log(`${name}${age}${gender}`);
+};
+sayhi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,12 @@
+const name = "Nicolas",
+age = 4,
+gender="male"
+
+
+const sayhi = (name, age, gender?) => {
+console.log(`${name}${age}${gender}`)
+}
+
+sayhi(name, age);
+
+export{}; 
